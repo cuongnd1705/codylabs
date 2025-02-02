5c16fbd357a73e5de2a133aca3fd3df9a2e1ec80
cuongnd <cuongnd.work@gmail.com>
2025-02-02 18:49:51 +0700

    feat(case-converter): add jest config

---
+++ packages/case-converter/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+
+export default async (): Promise<Config> => {
+  return {
+    collectCoverageFrom: ['**/*.(t|j)s'],
+    coverageDirectory: '../coverage',
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: 'src',
+    testEnvironment: 'node',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+      '^.+\\.(t|j)s$': 'ts-jest',
+    },
+  };
+};
